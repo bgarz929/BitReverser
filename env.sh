9dae9ac912d21574fa70f12d28ec7d12c0d3386b
bgarz929 <bangaris929@gmail.com>
2026-01-21 20:38:51 -0800

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
-export PATH=/usr/local/cuda-9.2/bin${PATH:+:${PATH}}
-export LD_LIBRARY_PATH=/usr/local/cuda-9.2/lib64 ${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
